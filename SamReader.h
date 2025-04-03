7ac6371b4e5e96c489059d3ce0af541de3177af7
egor-shevtsov <Egor.Shevtsov@fer.hr>
2025-04-03 22:31:29 +0200

    Preparations for the calculation of the rate errors are implemented. Barely tested

--- SamReader.h
+++ SamReader.h
@@ -1,10 +1,24 @@
 #ifndef SAMREADER_H
 #define SAMREADER_H
 #include <string>
+#include <boost/icl/interval_map.hpp>
+#include <utility>
+
+using namespace std;
+using namespace boost::icl;
+
+struct AlignmentMaps {
+	interval_map<size_t, std::string> intervalAlignment;
+	std::map<size_t, size_t> insertionCount;
+
+	AlignmentMaps(interval_map<size_t, std::string> alignments, std::map<size_t, size_t> insertions): intervalAlignment(std::move(alignments)), insertionCount(std::move(insertions)) {}
+};
 
 class SamReader {
 public:
-	static int getAlignments(const std::string& fileName);
+	static AlignmentMaps getAlignments(const std::string& fileName);
+	static char complementSymbol(const char &n);
+	static void reverseComplement(std::string &read);
 };
 
 #endif //SAMREADER_H
--- main.cpp
+++ main.cpp
@@ -6,4 +6,5 @@
 int main(int argc, char *argv[]) {
   const std::string fullPath = std::filesystem::current_path().parent_path().string() + '/' + argv[1];
   auto alignments = SamReader::getAlignments(fullPath);
+  cout << 2 << std::endl;
 }
